dd69ca3ac3f0c28b63e79d6b536f8b5e58c5c470
Rmg91 <romerg1691@gmail.com>
2026-06-07 21:33:31 -0500

    fixed
    fixed

--- script.js
+++ script.js
@@ -64,6 +64,7 @@ async function cargarDatos() {
       <td>${persona["CF 6 May"]}</td>
       <td>${persona["CF 18 May"]}</td>
       <td>${persona["CF 21 May"]}</td>
+      <td>${persona["CF 7 Jun"]}</td>
     `;
     tbody.appendChild(tr);
   });
